71a53a49620cb78be3dd5e05cf2ee7fa761743b6
Vitalina Dushchak <vitalina.dushchak@eleks.com>
2023-04-25 18:22:16 +0300

    deleted Author column from dbo.Books

--- BookishMadness.DAL/Entities/Book.cs
+++ BookishMadness.DAL/Entities/Book.cs
@@ -7,7 +7,6 @@ namespace BookishMadness.DAL.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        //public string Author { get; set; }
         public int PagesCount { get; set; }
         public string Genre { get; set; }
         public BookStatus Status { get; set; }
---
+++ books-db/dbo/Migrations/04-25-2023_Remove_Author_Column_From_[dbo].[Books].sql
@@ -0,0 +1,8 @@
+﻿IF EXISTS(SELECT * FROM sys.columns 
+          WHERE Name = N'Author'
+          AND Object_ID = Object_ID(N'dbo.Books'))
+
+          BEGIN
+             ALTER TABLE [dbo].[Books]
+             DROP COLUMN Author
+          END
