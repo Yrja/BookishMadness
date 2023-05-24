36e77dcf73191a2b19e29f85becc375ca5051927
Vitalina Dushchak <vitalina.dushchak@eleks.com>
2023-05-24 21:16:51 +0300

    added stored procedure and function to get book summary

---
+++ books-db/dbo/Functions/fn_BookSummary.sql
@@ -0,0 +1,36 @@
+﻿CREATE FUNCTION [dbo].[fn_BookSummary]
+(
+	 @dateFrom datetime
+	,@dateTo datetime
+	,@bookId uniqueidentifier
+)
+RETURNS TABLE
+AS
+	RETURN
+	WITH tbl AS(
+		SELECT
+			 books.Id BookId
+			,Round(AVG(Cast(reactions.Reaction AS Float)), 4) AvgReaction
+			,COUNT(reactions.Reaction) ReactionsCount
+			,SUM(reactions.Reaction) LikesQuantity
+			,COUNT(reactions.Reaction) - SUM(reactions.Reaction) DislikesQuantity
+		FROM dbo.Books AS books
+			LEFT JOIN 
+			(select * from dbo.Reactions
+			 where CreatedOn >= @dateFrom and CreatedOn <= @dateTo) reactions
+		ON reactions.BookId = books.Id
+		GROUP BY books.Id)
+
+	SELECT 
+		 books.Id
+		,books.Name
+		,dbo.fn_FullName(authors.Name, authors.Surname) AuthorFullName
+		,tbl.AvgReaction
+		,tbl.ReactionsCount
+		,LikesQuantity
+		,DislikesQuantity
+	FROM tbl
+	LEFT JOIN dbo.Books books ON tbl.BookId = books.Id
+	LEFT JOIN dbo.Authors authors ON books.AuthorId = authors.Id
+	WHERE BookId = @bookId
+GO
